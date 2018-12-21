b6609114e8dd4ff7e416be5586aaf43fa6476f46
wangtian <wangtian2013@live.com>
2018-12-21 14:23:13 +0800

    update shell format

--- scripts/win/active.bat
+++ scripts/win/active.bat
@@ -7,7 +7,7 @@ set service_name=CONSUL
 
 set consul_bin_path=%base_dir%%consul_bin_name%
 set consul_config_path=%base_dir%%consul_config_file%
-set log_path=%base_dir%log\\consul.log
+set log_path=%base_dir%log\consul.log
 
 @echo on
 sc create %service_name% binpath="%consul_bin_path% agent -config-file=%consul_config_path% -log-file=%log_path%" type=own displayname=%service_name% start=auto
